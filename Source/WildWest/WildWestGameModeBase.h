e62555f4a31f05f42586af847d30fffc23b46826
DY <dave1315@korea.ac.kr>
2022-12-30 00:36:36 +0900

    프로젝트 생성

---
+++ Source/WildWest/WildWestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WildWestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDWEST_API AWildWestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
